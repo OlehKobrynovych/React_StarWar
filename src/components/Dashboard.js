7b8a9607b854957ea9fd2b3f89529c08e6c29185
OlehKobrynovych <o.kobrynovych@gmail.com>
2022-05-29 20:19:19 +0300

    added content

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,30 @@
 import React from "react";
+import darck from "../img/darck.jpg";
+import light from "../img/light.jpg";
+import { useNavigate } from "react-router-dom";
+
+
 
 
 const Dashboard = () => {
     console.log('Dashboard');
+    const navigate = useNavigate();
+
+    const handlerChang = (el) => {
+        navigate(`/${el}`)
+    }
 
     return (
-        <div>
-            Dashboard
+        <div className="dashboard" >
+            {/* <div className="dashboard__title">Star War</div> */}
+            <div className="dashboard__title">Star War</div>
+                <p className="dashboard__ask" >Who do you want to see???</p>
+            <div className='dashboard__btn-wrap' >
+                <button className="dashboard__btn" onClick={()=>handlerChang('peoples')} >Peoples</button>
+                <button className='dashboard__btn' onClick={()=>handlerChang('planets')} >Planets</button>
+            </div>
+            <img className='dashboard__theme-btn' src={darck} />
+            <img className='dashboard__theme-btn' src={light} />
         </div>
     )
 }
